4323be4f419cc12a4caecdf9f773f1c42a3a382c
plankkton <leonan_nogueira@hotmail.com>
2021-04-01 14:06:20 -0300

    testes com sort e group by com array

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,36 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach{println(it)}
+
+    println("----------------------------------")
+    println(funcionarios.find { it.nome == "Maria" })
+
+    println("----------------------------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("----------------------------------")
+    funcionarios
+        .groupBy { it.tipoContrato }
+        .forEach { println(it) }
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContrato: String
+){
+    override fun toString(): String =
+        """
+            nome:    $nome
+            Salário: $salario
+        """.trimIndent()
+}
